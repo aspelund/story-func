101b0f3657a02577fc1361c47a2551457d038964
Aspelund <mattias.aspelund@gmail.com>
2022-05-03 12:58:32 +0200

    Rebuilt

--- build/src/types.js
+++ build/src/types.js
@@ -1,8 +1,7 @@
-var PipeTypes;
+export var PipeTypes;
 (function (PipeTypes) {
     PipeTypes[PipeTypes["when"] = 0] = "when";
     PipeTypes[PipeTypes["then"] = 1] = "then";
     PipeTypes[PipeTypes["given"] = 2] = "given";
     PipeTypes[PipeTypes["story"] = 3] = "story";
 })(PipeTypes || (PipeTypes = {}));
-export { PipeTypes };
